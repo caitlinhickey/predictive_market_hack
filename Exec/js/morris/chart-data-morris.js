ba5de208a99d2cfcf4f8c0fb8c80cfc7b5c35514
Caitlin Hickey <caitlin.e.hickey@colorado.edu>
2014-07-18 00:23:28 -0700

    new stuff

--- Exec/js/morris/chart-data-morris.js
+++ Exec/js/morris/chart-data-morris.js
@@ -6,37 +6,11 @@ Morris.Area({
   // Chart data records -- each entry in this array corresponds to a point on
   // the chart.
   data: [
-	{ d: '2012-10-01', visits: 802 },
-	{ d: '2012-10-02', visits: 783 },
-	{ d: '2012-10-03', visits:  820 },
-	{ d: '2012-10-04', visits: 839 },
-	{ d: '2012-10-05', visits: 792 },
-	{ d: '2012-10-06', visits: 859 },
-	{ d: '2012-10-07', visits: 790 },
-	{ d: '2012-10-08', visits: 1680 },
-	{ d: '2012-10-09', visits: 1592 },
-	{ d: '2012-10-10', visits: 1420 },
-	{ d: '2012-10-11', visits: 882 },
-	{ d: '2012-10-12', visits: 889 },
-	{ d: '2012-10-13', visits: 819 },
-	{ d: '2012-10-14', visits: 849 },
-	{ d: '2012-10-15', visits: 870 },
-	{ d: '2012-10-16', visits: 1063 },
-	{ d: '2012-10-17', visits: 1192 },
-	{ d: '2012-10-18', visits: 1224 },
-	{ d: '2012-10-19', visits: 1329 },
-	{ d: '2012-10-20', visits: 1329 },
-	{ d: '2012-10-21', visits: 1239 },
-	{ d: '2012-10-22', visits: 1190 },
-	{ d: '2012-10-23', visits: 1312 },
-	{ d: '2012-10-24', visits: 1293 },
-	{ d: '2012-10-25', visits: 1283 },
-	{ d: '2012-10-26', visits: 1248 },
-	{ d: '2012-10-27', visits: 1323 },
-	{ d: '2012-10-28', visits: 1390 },
-	{ d: '2012-10-29', visits: 1420 },
-	{ d: '2012-10-30', visits: 1529 },
-	{ d: '2012-10-31', visits: 1892 },
+	{ d: '2014-07-03', visits: 50 },
+	{ d: '2014-07-04', visits: 60 },
+	{ d: '2014-07-05', visits: 65 },
+	{ d: '2014-07-06', visits: 70 },
+	{ d: '2014-07-08', visits: 73 }
   ],
   // The name of the data record attribute that contains x-visitss.
   xkey: 'd',
@@ -44,7 +18,7 @@ Morris.Area({
   ykeys: ['visits'],
   // Labels for the ykeys -- will be displayed when you hover over the
   // chart.
-  labels: ['Visits'],
+  labels: ['Sold At'],
   // Disables line smoothing
   smooth: false,
 });
@@ -53,7 +27,7 @@ Morris.Donut({
   element: 'morris-chart-donut',
   data: [
     {label: "For Sale", value: 42.7},
-    {label: "Being Held", value: 57.3},
+    {label: "Being Held", value: 57.3}
   ],
   formatter: function (y) { return y + "%" ;}
 });
