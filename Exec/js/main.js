e196368b529fdc84f26093580da2134e71fc3368
Harsha Yeddanapudy <Harsha.Yeddanapudy@viasat.com>
2014-07-17 22:02:12 -0600

    some commits

--- Exec/js/main.js
+++ Exec/js/main.js
@@ -3,9 +3,9 @@ function showTransaction(json) {
         entry = json.feed.entry[i];
         $('#trans').append(
             '<tr class="active">' +
-            '<td>'+ entry.gsx$seller.$t +'</td>'+
+            '<div id="theTog" class="TogWrap"><td>'+ entry.gsx$seller.$t +'</td><p class="togContent"> '+ entry.gsx$seldep.$t +'</p></div>'+
             '<td>'+ entry.gsx$seldep.$t +'</td>'+
-            '<td>'+ entry.gsx$buyer.$t +'</td>'+
+            '<div id="theTog" class="TogWrap"><td>'+ entry.gsx$buyer.$t +'</td><p class="togContent"> hello </p></div>'+
             '<td>'+ entry.gsx$buydep.$t +'</td>'+
             '<td>'+ entry.gsx$amnt.$t +'</td>'+
             '<td>'+ entry.gsx$time.$t +'</td>'+
